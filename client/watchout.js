5ae0454f52462aae119a8c6a404aca3f2f8955e9
Lamiaa Al Olabi <Lamiaa.olabi@gmail.com>
2016-06-16 19:27:28 +0300

    Styling & CSS, Added a gif as the player, Added a gif as enemies.

--- client/watchout.js
+++ client/watchout.js
@@ -7,33 +7,33 @@ var svgContainer = d3.select("body")
 					.style('border', '10px solid')
 					.style('background-color','orange');
 
-
 var minX = 10;
 var maxX= 490;
 var minY = 10;
 var maxY= 490;
 
-var player = svgContainer.append('circle')
-						.attr('cx', 200)
-						.attr('cy', 200)
-						.attr('r', 10)
-						.attr('fill', 'white')
+var player = svgContainer.append('image')
+						.attr('x', 200)
+						.attr('y', 200)
+						.attr('xlink:href','Running.gif')
+						.attr('height', 50)
+						.attr('width', 50)
 						.attr('id', 'player')
 						.call(d3.behavior.drag()  
              					.on('dragstart', function() { player.style('fill', 'red'); })
              					.on('drag', function() {
-             					 player.attr('cx', d3.event.x).attr('cy', d3.event.y); 
-             					if (parseFloat(player.attr('cx'))> maxX) {
-									player.attr('cx', maxX)
+             					 player.attr('x', d3.event.x).attr('y', d3.event.y); 
+             					if (parseFloat(player.attr('x'))> maxX) {
+									player.attr('x', maxX)
 								}
-								if (parseFloat(player.attr('cx'))< minX) {
-									player.attr('cx', minX)
+								if (parseFloat(player.attr('x'))< minX) {
+									player.attr('x', minX)
 								}
-								if (parseFloat(player.attr('cy'))> maxY) {
-									player.attr('cy', maxY)
+								if (parseFloat(player.attr('y'))> maxY) {
+									player.attr('y', maxY)
 								}
-								if (parseFloat(player.attr('cy'))< minY) {
-									player.attr('cy', minY)
+								if (parseFloat(player.attr('y'))< minY) {
+									player.attr('y', minY)
 								}})
 
              					.on('dragend', function() { player.style('fill', 'white');}))
@@ -54,14 +54,14 @@ for (var i=0; i<20; i++){
 }
 
 var enemy = svgContainer.selectAll('svg').data(enemies, function(d){return d.id});
-enemy.enter().append('circle').attr('cx', function(d){return d.x}).attr('cy', function(d){return d.y}).attr('r', 10)
+enemy.enter().append('image').attr('xlink:href','skull.gif').attr('x', function(d){return d.x}).attr('y', function(d){return d.y}).attr('width', 30).attr('height', 30)
 var detect_coll = function(){
 	var collide = false;
 	for (var i=0; i<enemy[0].length; i++){
-			var xPos = parseFloat(d3.select(enemy[0][i]).attr('cx'));
-			var yPos = parseFloat(d3.select(enemy[0][i]).attr('cy'));
-			var distance_x = Math.abs(xPos-parseFloat(player.attr('cx')));
-			var distance_y = Math.abs(yPos-parseFloat(player.attr('cy')));
+			var xPos = parseFloat(d3.select(enemy[0][i]).attr('x'));
+			var yPos = parseFloat(d3.select(enemy[0][i]).attr('y'));
+			var distance_x = Math.abs(xPos-parseFloat(player.attr('x')));
+			var distance_y = Math.abs(yPos-parseFloat(player.attr('y')));
 			var dist = Math.sqrt(Math.pow(distance_x, 2)+Math.pow(distance_y, 2));
 			if (dist < 10){
 				collide = true
@@ -84,8 +84,8 @@ function move_Enemy(){
 	enemy
 	.transition()
 	.duration(1500)
-	.attr('cx', function(){return Math.random()*500})
-	.attr('cy', function(){return Math.random()*500})
+	.attr('x', function(){return Math.random()*500})
+	.attr('y', function(){return Math.random()*500})
 };
 
 function incScore() {
